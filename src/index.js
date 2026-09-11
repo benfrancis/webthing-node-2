88cfed6bb864d49150e65c9f33febfc8b5c5e2e3
Ben Francis <ben@tola.me.uk>
2026-09-11 18:49:05 +0100

    Add a build script which generates .d.ts files for developers using the package with TypeScript

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as Thing } from './thing.js';
+export { default as ThingServer } from './thing-server.js';
+export { default as InteractionAffordance } from './interaction-affordance.js';
+export { default as PropertyAffordance } from './property-affordance.js';
+export { default as ValidationError } from './validation-error.js';
